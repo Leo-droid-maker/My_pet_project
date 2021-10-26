9d0b316cb03f8f79610ce48baf29325bc1ee50cb
Leo-droid-maker <jwabeljr@gmail.com>
2021-10-26 12:41:00 +0300

    dotenv added

--- my_project/myproject/myproject/settings.py
+++ my_project/myproject/myproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import json
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('myproject/secret_key.json', 'r') as file_with_sk:
-    sk = json.load(file_with_sk)
+load_dotenv()
 
-SECRET_KEY = sk['SECRET_KEY']
+SECRET_KEY = os.getenv("SECRET_KEY", default='Key file does not exists')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
